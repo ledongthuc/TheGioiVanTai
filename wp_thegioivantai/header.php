232e454549280fa3f17068531f8c57829e306fe0
tuanphunglk <tuanphunglk@gmail.com>
2013-02-03 14:40:30 +0700

    remove ref jquery.js in header, move to plugin

--- wp_thegioivantai/header.php
+++ wp_thegioivantai/header.php
@@ -13,7 +13,7 @@
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
-<script src="<?php bloginfo('template_directory');?>/slide/slides.min.jquery.js"></script>
+
 <?php wp_head(); ?>
 
 </head>
